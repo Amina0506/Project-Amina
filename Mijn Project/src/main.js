ad727905aeae7b5a571217aa2d5c955a74598319
Amina0506 <amina010506@gmail.com>
2025-05-05 16:51:18 +0200

    script verwerkt

--- Mijn Project/src/main.js
+++ Mijn Project/src/main.js
@@ -1,24 +1,24 @@
 // Alle item-divs selecteren
-  const itemDivs = document.querySelectorAll('.pokemon-list .list > div');
- 
-  // Haal 20 Pokémon op
-  fetch('https://pokeapi.co/api/v2/pokemon?limit=20')
-    .then(response => response.json())
-    .then(data => {
-      const pokemonList = data.results;
- 
-      // Voor elke Pokémon details ophalen
-      const promises = pokemonList.map(pokemon => fetch(pokemon.url).then(res => res.json()));
-      Promise.all(promises).then(pokemonDetails => {
-        // Itereer over de bestaande divs en vul ze met data
-        pokemonDetails.forEach((pokemon, index) => {
-          const div = itemDivs[index];
-          if (div) {
-            div.innerHTML = `
-<img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" style="width:100px;">
+const itemDivs = document.querySelectorAll('.pokemon-list .list > div');
+
+// Haal 20 Pokémon op
+fetch('https://pokeapi.co/api/v2/pokemon?limit=20')
+  .then(response => response.json())
+  .then(data => {
+    const pokemonList = data.results;
+
+    // Voor elke Pokémon details ophalen
+    const promises = pokemonList.map(pokemon => fetch(pokemon.url).then(res => res.json()));
+    Promise.all(promises).then(pokemonDetails => {
+      // Itereer over de bestaande divs en vul ze met data
+      pokemonDetails.forEach((pokemon, index) => {
+        const div = itemDivs[index];
+        if (div) {
+          div.innerHTML = `
+<img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" style="width:200px;">
 <h2>${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h2>
             `;
-          }
-        });
+        }
       });
     });
+  });
